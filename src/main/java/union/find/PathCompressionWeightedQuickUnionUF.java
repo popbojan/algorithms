0ef5ed1930afed33dea64bc9f65b6d61ec24416a
pop <popbojan@yahoo.com>
2018-02-26 06:54:33 +0100

    Practice path compression algorithm

--- src/main/java/union/find/PathCompressionWeightedQuickUnionUF.java
+++ src/main/java/union/find/PathCompressionWeightedQuickUnionUF.java
@@ -6,8 +6,10 @@ public class PathCompressionWeightedQuickUnionUF extends WeightedQuickUnionUF {
         super(N);
     }
 
+    @Override
     protected int root(int i){
         while (i != id[i]){
+            id[i] = id[id[i]];
             i = id[i];
         }
         return i;
--- src/test/java/unit/union/find/PathCompressionWeightedQuickUnionUFTests.java
+++ src/test/java/unit/union/find/PathCompressionWeightedQuickUnionUFTests.java
@@ -24,13 +24,28 @@ public class PathCompressionWeightedQuickUnionUFTests {
     public void canBeCreated() {
         UF pcwqu = new PathCompressionWeightedQuickUnionUF(10);
     }
+
     @Test
     public void union_unitedObjectsAreConnected() {
         pcwqu.union(1,  5);
         pcwqu.union(7, 1);
 
+        pcwqu.union(0, 9);
+        pcwqu.union(4, 9);
+
+        pcwqu.printIdArray();
+
+        /** this union compresses the path, because root of 1 is 5, whereas root of 5 is 7
+         *  and afterwards root of 1 becomes 7
+         */
+        pcwqu.union(4, 1);
+
+        pcwqu.printIdArray();
+
+        assertTrue(pcwqu.connected(5, 9));
         assertTrue(pcwqu.connected(1, 5));
         assertTrue(pcwqu.connected(1, 7));
+        assertTrue(pcwqu.connected(4, 7));
 
         pcwqu.printIdArray();
     }
