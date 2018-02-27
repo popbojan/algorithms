9e3f3841bcce106afd87d5dbac5319a9daa24bc6
pop <popbojan@yahoo.com>
2018-02-27 07:06:12 +0100

    Introduce brute force algorithm for three sum

---
+++ src/main/java/brute/force/ThreeSum.java
@@ -0,0 +1,22 @@
+package brute.force;
+
+public class ThreeSum {
+
+    public int count(int[] a)
+    {
+        int N = a.length;
+        int count = 0;
+        for(int i=0; i<N; i++){
+            for(int j=i+1; j<N; j++){
+                for(int k=j+1; k<N; k++){
+                    System.out.print("a[i]:["+a[i]+"] a[j]: ["+a[j]+"] a[k]: ["+a[k]+"] ");
+                    System.out.print("\n");
+                    if(a[i] + a[j] + a[k] == 10){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
