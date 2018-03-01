f2525065d964030fbb8f473e6ba2ccf9a6eb7611
pop <popbojan@yahoo.com>
2018-03-01 07:01:26 +0100

    Test binary search algorithm

--- src/main/java/binary/search/BinarySearch.java
+++ src/main/java/binary/search/BinarySearch.java
@@ -1,4 +1,21 @@
 package binary.search;
 
+import java.util.Arrays;
+
 public class BinarySearch {
+
+    public int binarySearch(int[] a, int key) {
+        int lo = 0, hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (key < a[mid]){
+                hi = mid -1;
+            }
+            else if (key > a[mid]){
+                lo = mid +1;
+            }
+            else  return mid;
+        }
+        return -1;
+    }
 }
--- src/test/java/unit/binary/search/BinarySearchTests.java
+++ src/test/java/unit/binary/search/BinarySearchTests.java
@@ -1,4 +1,33 @@
 package unit.binary.search;
 
+import binary.search.BinarySearch;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
 public class BinarySearchTests {
+
+    BinarySearch bs;
+
+    @Before
+    public void setup()
+    {
+        MockitoAnnotations.initMocks(this);
+
+        bs = new BinarySearch();
+    }
+
+    @Test
+    public void canBeCreated() {
+        BinarySearch bs = new BinarySearch();
+    }
+
+    @Test
+    public void binarySearch_elementIsFound() {
+        int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int result = bs.binarySearch(a, 8);
+
+        Assert.assertEquals(7, result);
+    }
 }
