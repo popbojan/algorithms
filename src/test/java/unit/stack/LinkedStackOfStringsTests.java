c085085cefcae9d02a6152bda1a8c123c1fe956e
pop <popbojan@yahoo.com>
2018-03-02 07:12:47 +0100

    Introduce LinkedStackOfStrings

---
+++ src/test/java/unit/stack/LinkedStackOfStringsTests.java
@@ -0,0 +1,36 @@
+package unit.stack;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+import stack.LinkedStackOfStrings;
+
+public class LinkedStackOfStringsTests {
+
+    LinkedStackOfStrings linkedStack;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        linkedStack = new LinkedStackOfStrings();
+    }
+
+    @Test
+    public void canBeCreated() {
+        LinkedStackOfStrings linkedStack = new LinkedStackOfStrings();
+    }
+
+    @Test
+    public void push_ItemsAreLinked(){
+        linkedStack.push("!!!");
+        linkedStack.push("best");
+        linkedStack.push("the");
+        linkedStack.push("is");
+        linkedStack.push("Bojan");
+
+        while(!linkedStack.isEmpty()){
+            System.out.println(linkedStack.pop());
+        }
+    }
+}
