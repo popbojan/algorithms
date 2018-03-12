e8a6c6122cec5305e34cc6ae8670464ea856b5b5
pop <popbojan@yahoo.com>
2018-03-12 07:38:11 +0100

    [a3] Introduce Dijkstra s two stack algorithm

---
+++ src/test/java/unit/stack/Dijkstra_sTwoStackAlgorithmTests.java
@@ -0,0 +1,23 @@
+package unit.stack;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import stack.Dijkstra_sTwoStackAlgorithm;
+
+public class Dijkstra_sTwoStackAlgorithmTests {
+
+    Dijkstra_sTwoStackAlgorithm da;
+
+    @Before
+    public void setup(){
+        da = new Dijkstra_sTwoStackAlgorithm();
+    }
+
+    @Test
+    public void calculateExpression(){
+        Double result = da.calculate("(((2+3)+7)*2)");
+        Assert.assertEquals((Double)(24.0), result);
+    }
+
+}
