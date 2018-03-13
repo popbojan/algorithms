b460864a6099ef27f312dfcd3d613cb7e5407039
pop <popbojan@yahoo.com>
2018-03-13 07:34:20 +0100

    [a4] Introduce SelectionSort

---
+++ src/main/java/sort/SelectionSort.java
@@ -0,0 +1,18 @@
+package sort;
+
+public class SelectionSort extends Sort {
+
+    @Override
+    public void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exchange(a, i, min);
+        }
+    }
+}
