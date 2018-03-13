8e32cf6810917cbe50e1a2881b93af90b32efad1
pop <popbojan@yahoo.com>
2018-03-13 07:21:54 +0100

    [a3] Introduce sort API

---
+++ src/main/java/sort/Sort.java
@@ -0,0 +1,24 @@
+package sort;
+
+public class Sort {
+
+    public boolean less(Comparable v, Comparable w){
+        return v.compareTo(w) < 0;
+    }
+
+    public void exchange(Comparable[] a, int i, int j){
+        Comparable swap =  a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    public boolean isSorted(Comparable[] a){
+        for(int i=1; i<a.length; i++){
+            if(less(a[i], a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
