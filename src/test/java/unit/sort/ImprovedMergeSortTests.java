9663eb17026becedfd6cdde088bd39a9ef27dccb
pop <popbojan@yahoo.com>
2018-03-30 07:20:29 +0200

    [a4] Introduce ImprovedMergeSort - it doesn't work quite well

--- src/test/java/unit/sort/ImprovedMergeSortTests.java
+++ src/test/java/unit/sort/ImprovedMergeSortTests.java
@@ -1,4 +1,31 @@
 package unit.sort;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import sort.ImprovedMergeSort;
+import sort.Sort;
+
 public class ImprovedMergeSortTests {
+
+    Sort ims;
+
+    @Before
+    public void setup() {
+        ims = new ImprovedMergeSort();
+    }
+
+    @Test
+    public void test_merge() {
+        Comparable[] a = {1, 2, 3, 4, 5, 7, 8, 9, 6, 11};
+
+        ims.exchange(a, 1, 3);
+        ims.sort(a);
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+
+        Assert.assertTrue(ims.isSorted(a));
+    }
 }
