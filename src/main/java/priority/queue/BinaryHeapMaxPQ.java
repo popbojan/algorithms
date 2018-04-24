0eef4b04e841a0cd9a812fcfaa0dcc6dd68ded46
pop <popbojan@yahoo.com>
2018-04-24 07:05:04 +0200

    [a6] Introducee BinaryHeapMaxPQ

--- src/main/java/priority/queue/BinaryHeapMaxPQ.java
+++ src/main/java/priority/queue/BinaryHeapMaxPQ.java
@@ -1,43 +1,57 @@
 package priority.queue;
 
-public class BinaryHeapMaxPG<Key extends Comparable<Key>> extends UnorderedMaxPQ {
+import sort.Sort;
 
-    public BinaryHeapMaxPG(int capacity) {
-        super(capacity);
+public class BinaryHeapMaxPQ<Key extends Comparable<Key>> extends Sort {
+
+    protected Key[] pq;
+    protected int N;
+
+    public BinaryHeapMaxPQ(int capacity) {
+        pq = (Key[]) new Comparable[capacity + 1];
     }
 
-    public Key delMax(){
-        Key max = (Key) pq[1];
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+
+    public Key delMax() {
+        Key max = pq[1];
         exchange(pq, 1, N--);
         sink(1);
-        pq[N+1] = null; // prevent loitering
+        pq[N + 1] = null; // prevent loitering
         return max;
     }
 
-    public void myInsert(Key x) {
-        super.insert(x);
-//        pq[N++] =x;
+    public void insert(Key x) {
+        pq[++N] = x;
         swim(N);
     }
 
-    private void swim(int k){
-        while(k > 1 && less(k/2, k)){
-            exchange(pq, k, k/2);
-            k = k/2;
+    private void swim(int k) {
+        while (k > 1 && less(pq, k / 2, k)) {
+            exchange(pq, k, k / 2);
+            k = k / 2;
         }
     }
 
-    private void sink(int k){
-        while(2*k <= N){
-            int j = 2*k;
-            if(j < N && less(j, j++)){
+    private void sink(int k) {
+        while (2 * k <= N) {
+            int j = 2 * k;
+            if (j < N && less(pq, j, j + 1)) {
                 j++;
             }
-            if(!less(k, j)){
+            if (!less(pq, k, j)) {
                 break;
             }
             exchange(pq, k, j);
             k = j;
         }
+    }
+
+    @Override
+    public void sort(Comparable[] a) {
+
     }
 }
--- src/test/java/unit/priority/queue/BinaryHeapMaxPQTests.java
+++ src/test/java/unit/priority/queue/BinaryHeapMaxPQTests.java
@@ -1,4 +1,27 @@
 package unit.priority.queue;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import priority.queue.BinaryHeapMaxPQ;
+
 public class BinaryHeapMaxPQTests {
+
+    BinaryHeapMaxPQ<Integer> bhmpq;
+
+    @Before
+    public void setup() {
+        bhmpq = new BinaryHeapMaxPQ(5);
+    }
+
+    @Test
+    public void test_delMax() {
+        bhmpq.insert(0);
+        bhmpq.insert(4);
+        bhmpq.insert(2);
+        bhmpq.insert(8);
+        bhmpq.insert(5);
+
+        Assert.assertEquals((Integer) 8, bhmpq.delMax());
+    }
 }
