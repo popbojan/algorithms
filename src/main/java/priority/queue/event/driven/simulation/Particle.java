5d9bd3fd7894f2c7d16c641a7ebf4533b6428aee
pop <popbojan@yahoo.com>
2018-05-24 07:03:25 +0200

    [a6] Implement timeToHit method

--- src/main/java/priority/queue/event/driven/simulation/Particle.java
+++ src/main/java/priority/queue/event/driven/simulation/Particle.java
@@ -2,9 +2,28 @@ package priority.queue.event.driven.simulation;
 
 public class Particle extends Ball {
 
+    protected final double INFINITY = 0.0;
+
     // predict collision with particle or wall
     public double timeToHit(Particle that){
-        throw new  RuntimeException("not implemented yet!");
+        if(this == that){
+            return INFINITY;
+        }
+        double dx = that.rx - this.rx, dy = that.ry - this.ry;
+        double dvx = that.vx - this.vx, dvy = that.vy - this.vy;
+        double dvdr = dx*dvx + dy*dvy;
+        if(dvdr > 0)
+        {
+            return INFINITY;
+        }
+        double dvdv = dvx*dvx + dvy*dvy;
+        double drdr = dx*dx + dy*dy;
+        double sigma = this.RADIUS + that.RADIUS;
+        double d = (dvdr*dvdr) -dvdv *(drdr - sigma*sigma);
+        if(d < 0){
+            return INFINITY;
+        }
+        return -(dvdr + Math.sqrt(d)) / dvdv;
     }
 
     public double timeToHitVerticalWall(){
