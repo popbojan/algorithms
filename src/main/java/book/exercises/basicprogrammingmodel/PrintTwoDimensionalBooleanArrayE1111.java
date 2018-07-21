465cb58651e9903a03077afcf7ae31e26b5285f6
Pop <popbojan@yahoo.com>
2018-07-21 22:00:21 +0200

    [b1] Introduce skeleton for the exercise 1.1.11

---
+++ src/main/java/book/exercises/basicprogrammingmodel/PrintTwoDimensionalBooleanArrayE1111.java
@@ -0,0 +1,8 @@
+package book.exercises.basicprogrammingmodel;
+
+public class PrintTwoDimensionalBooleanArrayE1111 {
+
+    public void printArray(){
+
+    }
+}
---
+++ src/test/java/unit/book/exercises/basicprogrammingmodel/PrintTwoDimensionalBooleanArrayE1111Tests.java
@@ -0,0 +1,14 @@
+package unit.book.exercises.basicprogrammingmodel;
+
+import book.exercises.basicprogrammingmodel.PrintTwoDimensionalBooleanArrayE1111;
+import org.junit.Before;
+
+public class PrintTwoDimensionalBooleanArrayE1111Tests {
+
+    PrintTwoDimensionalBooleanArrayE1111 ptdba;
+
+    @Before
+    public void setup(){
+        ptdba.printArray();
+    }
+}
