b8a6a63956003c91347467127369fa32930d560a
Pop <popbojan@yahoo.com>
2018-07-21 19:42:54 +0200

    [b1] Do Exercise 1.1.7 (print values from some expressions)

---
+++ src/main/java/book/exercises/basicprogrammingmodel/PrintValuesE117.java
@@ -0,0 +1,38 @@
+package book.exercises.basicprogrammingmodel;
+
+import edu.princeton.cs.introcs.StdOut;
+
+public class PrintValuesE117 {
+
+    public void printA() {
+        double t = 9.0;
+        while (Math.abs(t - 9.0 / t) > .001)
+            t = (9.0 / t + t) / 2.0;
+        StdOut.printf("%.7f\n", t);
+    }
+
+    /**
+     * it sums up all the numbers form 1 to 10-1
+     * (because array starts with index 1)
+     * e.g 1+2+3+4+5+6+7+8+9
+     */
+    public void printB() {
+        int sum = 0;
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                sum++;
+            }
+        }
+        StdOut.println(sum);
+    }
+
+    public void printC() {
+        int sum = 0;
+        for (int i = 1; i < 5; i *= 2) {
+            for (int j = 0; j < 1000; j++) {
+                sum++;
+            }
+        }
+        StdOut.println(sum);
+    }
+}
---
+++ src/test/java/unit/book/exercises/basicprogrammingmodel/PrintValuesE117Tests.java
@@ -0,0 +1,31 @@
+package unit.book.exercises.basicprogrammingmodel;
+
+import book.exercises.basicprogrammingmodel.PrintValuesE117;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PrintValuesE117Tests {
+
+    PrintValuesE117 pv;
+
+    @Before
+    public void setup()
+    {
+        pv = new PrintValuesE117();
+    }
+
+    @Test
+    public void test_printA(){
+        pv.printA();
+    }
+
+    @Test
+    public void test_printB(){
+        pv.printB();
+    }
+
+    @Test
+    public void test_printC(){
+        pv.printC();
+    }
+}
