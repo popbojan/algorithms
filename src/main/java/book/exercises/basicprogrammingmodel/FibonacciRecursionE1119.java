28d1b09214c5ee7e377a4328f1791d95d0e27b6b
Pop <popbojan@yahoo.com>
2018-07-23 07:04:29 +0200

    [b1] Add comments to 1119 Fibonacci recursion

--- src/main/java/book/exercises/basicprogrammingmodel/FibonacciRecursionE1119.java
+++ src/main/java/book/exercises/basicprogrammingmodel/FibonacciRecursionE1119.java
@@ -2,6 +2,44 @@ package book.exercises.basicprogrammingmodel;
 
 import edu.princeton.cs.introcs.StdOut;
 
+/**
+ * result
+ * 0 0
+ * 1 1
+ * 2 1
+ * 3 2
+ * 4 3
+ * 5 5
+ * 6 8
+ * 7 13
+ * 8 21
+ * 9 34
+ *
+ * What is the secret?
+ * The thing is that each recursion (N-1) has it's own subtraction to 1 or 0
+ * however each of these steps has also recursion with (N-2) before 1 or 0 is reached
+ * For instance when N =5 we have:
+ * F(5-1) + F(5-2) then
+ * F(4-1) + F(4-2) then
+ * F(3-1) + F(3-2) then
+ * F(2-1) + F(2-2) then
+ * F(2-1) returns 1 then
+ * F(2-2) returns 0 then
+ * F(3-2) returns 1 then
+ * F(4-2) computes:
+ * F(2-1) + F(2-2) then
+ * F(2-1) returns 1 then
+ * F(2-2) returns 0 then
+ * F(5-2) computes:
+ * F(3-1) + F(3-2) then
+ * F(2-1) + F(2-2) then
+ * F(2-1) returns 1 then
+ * F(2-2) returns 0 then
+ * F(3-2) computes:
+ * F(3-2) returns 1
+ *
+ *
+ */
 public class FibonacciRecursionE1119 {
 
     private long F(int N) {
