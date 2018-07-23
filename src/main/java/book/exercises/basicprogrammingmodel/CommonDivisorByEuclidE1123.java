7d5be282406d48a3b7cbf859ad4ea7c8f041db40
Pop <popbojan@yahoo.com>
2018-07-23 14:26:21 +0200

    [b1] Append comment about quotient mod to 1124 and 1125

--- src/main/java/book/exercises/basicprogrammingmodel/CommonDivisorByEuclidE1123.java
+++ src/main/java/book/exercises/basicprogrammingmodel/CommonDivisorByEuclidE1123.java
@@ -12,9 +12,16 @@ public class CommonDivisorByEuclidE1123 {
         StdOut.println(gcd(p, q));
     }
 
+    /**
+     * Note* When dividend(e.g. 8) is smaller than divisor (e.g.32),
+     * then mod is always equal to the dividend, because
+     * bigger number cannot be inserted into smaller
+     * and get quotient
+     * e.g 7-inches screw cannot be inserted into 3-inches metal hole
+     */
     private int gcd(int p, int q) {
         StdOut.println("p: [" + p + "]  q: [" + q + "]");
-        if(q == 0) {
+        if (q == 0) {
             return p;
         }
         int r = p % q;
