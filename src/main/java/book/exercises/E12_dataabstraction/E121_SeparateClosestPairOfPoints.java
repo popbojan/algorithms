3f7d9dbd398bf13e1dd218a70bd7d187105731ab
pop <popbojan@yahoo.com>
2019-03-13 07:33:47 +0100

    [E121] Add basic construct

--- src/main/java/book/exercises/E12_dataabstraction/E121_SeparateClosestPairOfPoints.java
+++ src/main/java/book/exercises/E12_dataabstraction/E121_SeparateClosestPairOfPoints.java
@@ -1,4 +1,6 @@
 package book.exercises.E12_dataabstraction;
 
 public class E121_SeparateClosestPairOfPoints {
+
+    private final int squareEdgeLength = 300;
 }
--- src/test/java/unit/book/exercises/E12_dataabstraction/E121_SeparateClosestPairOfPointsTests.java
+++ src/test/java/unit/book/exercises/E12_dataabstraction/E121_SeparateClosestPairOfPointsTests.java
@@ -1,4 +1,24 @@
 package unit.book.exercises.E12_dataabstraction;
 
+import book.exercises.E12_dataabstraction.E121_SeparateClosestPairOfPoints;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
 public class E121_SeparateClosestPairOfPointsTests {
+
+    private E121_SeparateClosestPairOfPoints scpop;
+    private final int N = 10;
+
+    @Before
+    public void setup()
+    {
+        MockitoAnnotations.initMocks(this);
+
+        scpop = new E121_SeparateClosestPairOfPoints();
+    }
+
+    @Test
+    public void test() {
+    }
 }
