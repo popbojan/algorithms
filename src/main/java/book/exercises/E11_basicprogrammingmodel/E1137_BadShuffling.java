3d3404479d7e970e1cafcb4904172dcc488b9b31
pop <popbojan@yahoo.com>
2019-03-17 07:06:47 +0100

    [E1137] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1137_BadShuffling.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1137_BadShuffling.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 /**
  *
+ * Exercise 1.1.37:
  * Bad shuffling. Suppose that you choose a random integer between 0 and N-1
  * in our shuffling code instead of one between i and N-1. Show that resulting order is
  * not equally likely to be one of the N! possibilities. Run the test of the previous exercise
