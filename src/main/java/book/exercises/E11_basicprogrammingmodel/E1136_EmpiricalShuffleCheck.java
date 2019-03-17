7efdd0e4e00cc12f03f10dad8a08904fd49d4e0a
pop <popbojan@yahoo.com>
2019-03-17 07:11:55 +0100

    [E1136] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1136_EmpiricalShuffleCheck.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1136_EmpiricalShuffleCheck.java
@@ -2,6 +2,17 @@ package book.exercises.E11_basicprogrammingmodel;
 
 import java.util.Random;
 
+/**
+ *
+ * Exercise 1.1.36:
+ * Empirical shuffle check. Run computational experiments to check that our
+ * shuffling code on page 32 works as advertised. Write a program ShuffleTest that takes
+ * command-line arguments M and N, does N shuffles of an array of size M that is initialized
+ * with a[i] = i before each shuffle, and prints an M-by-M table such that row i
+ * gives the number of times i wound up in position j for all j. All entries in the array
+ * should be close to N/M.
+ *
+ */
 public class E1136_EmpiricalShuffleCheck {
 
     Random random;
