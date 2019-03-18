7dcf76f2813c604ad53da0eb975edc72b25f5358
pop <popbojan@yahoo.com>
2019-03-18 06:17:37 +0100

    [E1131] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1131_RandomConnections.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1131_RandomConnections.java
@@ -11,6 +11,15 @@ import javafx.stage.Stage;
 
 import java.util.List;
 
+/**
+ *
+ * Exercise 1.1.31:
+ * Random connections. Write a program that takes as command-line arguments
+ * an integer N and a double value p (between 0 and 1), plots N equally spaced
+ * dots of size .05 on the circumference of a circle, and then, with probability
+ * p for each pair of points, draws a gray line connecting them.
+ *
+ */
 public class E1131_RandomConnections extends Application {
 
     int N;
