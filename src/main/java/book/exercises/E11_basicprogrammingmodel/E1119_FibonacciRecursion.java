c436e5c75146d8c6c0f120735d9248243a62d079
pop <popbojan@yahoo.com>
2019-03-18 21:05:49 +0100

    [E1119] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1119_FibonacciRecursion.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1119_FibonacciRecursion.java
@@ -3,6 +3,31 @@ package book.exercises.E11_basicprogrammingmodel;
 import edu.princeton.cs.introcs.StdOut;
 
 /**
+ *
+ * Exercise 1.1.19:
+ * Run the following program on your computer:
+ *
+ * public class Fibonacci
+ * {
+ *     public static long F(int N)
+ *     {
+ *         if (N == 0) return 0;
+ *         if (N == 1) return 1;
+ *         return F(N-1) + F(N-2);
+ *     }
+ *
+ *     public static void main(String[] args)
+ *     {
+ *         for (int N = 0; N < 100; N++)
+ *             StdOut.println(N + " " + F(N));
+ *     }
+ * }
+ *
+ * What is the largest value of N for which this program takes less 1 hour to compute the
+ * value of F(N)? Develop a better implementation of F(N) that saves computed values in an array
+ *
+ *
+ *
  * result
  * 0 0
  * 1 1
