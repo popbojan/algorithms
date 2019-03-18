666617083c1a67669b7e00f996eefbd15d463879
pop <popbojan@yahoo.com>
2019-03-18 06:42:58 +0100

    [E1124] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1124_CommonDivisorByEuclid.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1124_CommonDivisorByEuclid.java
@@ -2,6 +2,17 @@ package book.exercises.E11_basicprogrammingmodel;
 
 import edu.princeton.cs.introcs.StdOut;
 
+/**
+ *
+ * Exercise 1.1.24:
+ * Give the sequence of values of p and q that are computed when Euclid's algorithm
+ * is used to compute the greatest common divisor of 105 and 24. Extend the code given
+ * on page 4 to develop a program Euclid that takes two integers from the command line
+ * and computes their greatest common divisor, printing out the two arguments for each
+ * call on the recursive method. Use your program to compute the greatest common divisor
+ * or 1111111 and 1234567.
+ *
+ */
 public class E1124_CommonDivisorByEuclid {
 
     public void commonDivisor() {
