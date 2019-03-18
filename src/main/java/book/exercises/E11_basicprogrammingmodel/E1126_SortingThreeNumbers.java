184bc757c233428a029fe3fa2be4ffc5e8413a43
pop <popbojan@yahoo.com>
2019-03-18 06:37:16 +0100

    [E1126] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1126_SortingThreeNumbers.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1126_SortingThreeNumbers.java
@@ -1,5 +1,16 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ *
+ * Exercise 1.1.26:
+ * Sorting three numbers. Suppose that the variables a, b, c and t are all of the same
+ * numeric primitive type. Show that the following code puts a, b, c in ascending order:
+ *
+ * if (a > b) { t = a; a = b; b = t; }
+ * if (a > c) { t = a; a = c; c = t; }
+ * if (b > c) { t = b; b = c; c = t }
+ *
+ */
 public class E1126_SortingThreeNumbers {
 
     public void sort(int a, int b, int c){
