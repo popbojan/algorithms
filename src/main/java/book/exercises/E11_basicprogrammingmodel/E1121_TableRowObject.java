7cee73b96227876d155a795231a0bf2c1366c1b8
pop <popbojan@yahoo.com>
2019-03-18 20:57:02 +0100

    [E1121] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1121_TableRowObject.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1121_TableRowObject.java
@@ -1,5 +1,15 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ *
+ * Exercise 1.1.21:
+ * Write a program that reads in lines from standard input with each line containing
+ * a name and two integers and then uses printf() to print a table with a column of
+ * the names, the integers, and the result of dividing the first by the second, accurate
+ * to three decimal places. You could use a program like this to tabulate batting averages
+ * for baseball players or grades for students.
+ *
+ */
 public class E1121_TableRowObject {
 
     String name;
