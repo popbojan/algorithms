a444ef197710320bc9f4f4dc55146b122a3099e5
pop <popbojan@yahoo.com>
2019-03-19 06:26:30 +0100

    [E119] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E119_BinaryRepresentationOfIntegerToString.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E119_BinaryRepresentationOfIntegerToString.java
@@ -1,5 +1,11 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ *
+ * Exercise 1.1.9:
+ * Write a code fragment that puts the binary representation of a positive integer
+ * N into a String s
+ */
 public class E119_BinaryRepresentationOfIntegerToString {
 
     public String getBinaryStringOfGivenNumber(int N){
