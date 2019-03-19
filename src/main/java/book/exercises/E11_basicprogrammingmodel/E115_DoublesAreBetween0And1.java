bc76f7395f489c4dde8c976c03cf081c03a65674
pop <popbojan@yahoo.com>
2019-03-19 21:26:07 +0100

    [E115] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E115_DoublesAreBetween0And1.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E115_DoublesAreBetween0And1.java
@@ -1,5 +1,12 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ *
+ * Exercise 1.1.5:
+ * Write a code fragment that prints true if the double variables x and y are both
+ * strictly between 0 and 1 and false otherwise.
+ *
+ */
 public class E115_DoublesAreBetween0And1 {
 
     public boolean areBetween0And1(double[] doubles) {
