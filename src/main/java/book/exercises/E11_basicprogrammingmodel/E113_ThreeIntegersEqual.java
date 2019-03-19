7e496140d400c44dc47425950c19966a41ab436b
pop <popbojan@yahoo.com>
2019-03-19 21:30:21 +0100

    [E113] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E113_ThreeIntegersEqual.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E113_ThreeIntegersEqual.java
@@ -1,5 +1,10 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ * Exercise 1.1.3:
+ * Write a program that takes three integer command-line arguments and prints
+ * equal if all three are equal, and not equal otherwise.
+ */
 public class E113_ThreeIntegersEqual {
 
     public boolean areEqual(int first, int second, int third) {
