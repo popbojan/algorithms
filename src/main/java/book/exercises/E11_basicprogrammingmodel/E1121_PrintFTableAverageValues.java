210320f56a88a8090061705a9eea3f870f51dd54
pop <popbojan@yahoo.com>
2019-03-19 06:32:16 +0100

    [E1121] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1121_PrintFTableAverageValues.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1121_PrintFTableAverageValues.java
@@ -2,6 +2,16 @@ package book.exercises.E11_basicprogrammingmodel;
 
 import edu.princeton.cs.introcs.StdOut;
 
+/**
+ *
+ * Exercise 1.1.21:
+ * Write a program that reads in lines from standard input with each line containing
+ * a name and two integers and then uses printf() to print a table with a column of
+ * the names, the integers, and the result of dividing the first by the second, accurate
+ * to three decimal places. You could use a program like this to tabulate batting averages
+ * for baseball players or grades for students.
+ *
+ */
 public class E1121_PrintFTableAverageValues {
 
     public void printF(E1121_TableRowObject[] a) {
