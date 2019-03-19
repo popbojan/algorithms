800cf07f749ade17719440d99f8be36e5cedc3d2
pop <popbojan@yahoo.com>
2019-03-19 05:51:35 +0100

    [E1115] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1115_Histogram.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1115_Histogram.java
@@ -1,5 +1,15 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ *
+ * Exercise 1.1.15:
+ * Write a static method histogram() that takes an array a[] of int values and an
+ * integer M as arguments and returns an array of length M whose ith entry is the
+ * number of times the integer i appeared in the argument array. If the values in
+ * a[] are all between 0 and M-1, the sum of the values in the returned array should
+ * be equal to a.length.
+ *
+ */
 public class E1115_Histogram {
 
     public int[] histogram(int[] a, int M)
