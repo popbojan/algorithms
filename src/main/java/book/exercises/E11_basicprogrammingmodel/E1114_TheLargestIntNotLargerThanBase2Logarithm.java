bead8495826e8ce78b1d1f46766bb71cb79e0138
pop <popbojan@yahoo.com>
2019-03-19 05:53:57 +0100

    [E1114] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E1114_TheLargestIntNotLargerThanBase2Logarithm.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E1114_TheLargestIntNotLargerThanBase2Logarithm.java
@@ -1,5 +1,12 @@
 package book.exercises.E11_basicprogrammingmodel;
 
+/**
+ *
+ * Exercise 1.1.14:
+ * Write a static method lg() that takes an int value N as argument and returns
+ * the largest int not larger than the base-2 logarithm of N. Do not use Math.
+ *
+ */
 public class E1114_TheLargestIntNotLargerThanBase2Logarithm {
 
     private int myPower(int base, int exponent) {
