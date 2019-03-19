c1925353bb9924c80f173012f177bcffa7f89727
pop <popbojan@yahoo.com>
2019-03-19 21:23:38 +0100

    [E116] Update description

--- src/main/java/book/exercises/E11_basicprogrammingmodel/E116_PrintFibonacciNumbers.java
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/E116_PrintFibonacciNumbers.java
@@ -1,7 +1,22 @@
 package book.exercises.E11_basicprogrammingmodel;
 
-        import edu.princeton.cs.introcs.StdOut;
+import edu.princeton.cs.introcs.StdOut;
 
+/**
+ *
+ * Exercise 1.1.6
+ * What does the following program print?
+ *
+ *    int f = 0;
+ *    int g = 1;
+ *    for (int i = 0; i <= 15; i++)
+ *    {
+ *        StdOut.println(f)
+ *        f = f + g;
+ *        g = f - g;
+ *    }
+ *
+ */
 public class E116_PrintFibonacciNumbers {
 
     public void printIt() {
