71ec2ee8a8c8b60cc4bf3a269e6d69e6801292b1
pop <popbojan@yahoo.com>
2019-03-23 09:44:57 +0100

    [E122] Rename Interval1D to be Interval1DClient

--- src/test/java/unit/book/exercises/E12_dataabstraction/E122_Interval1DClientTests.java
+++ src/test/java/unit/book/exercises/E12_dataabstraction/E122_Interval1DClientTests.java
@@ -1,20 +1,20 @@
 package unit.book.exercises.E12_dataabstraction;
 
-import book.exercises.E12_dataabstraction.E122_Interval1D;
+import book.exercises.E12_dataabstraction.E122_Interval1DClient;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.MockitoAnnotations;
 
-public class E122_Interval1DTests {
+public class E122_Interval1DClientTests {
 
-    private E122_Interval1D i1d;
+    private E122_Interval1DClient i1dc;
     private final int N = 10;
 
     @Before
     public void setup() {
         MockitoAnnotations.initMocks(this);
 
-        i1d = new E122_Interval1D();
+        i1dc = new E122_Interval1DClient();
     }
 
     @Test
@@ -23,6 +23,6 @@ public class E122_Interval1DTests {
         double[][] arrayWithPairOfDoubleValues = new double[][]{{1.0, 2.2}, {2.7, 12.3}, {4.8, 5.1}, {6.0, 6.1}, {11.9, 10.3}, {10.9, 8.4},
                 {3.1, 3.3}, {8.7, 8.5}, {9.1, 6.1}, {7.0, 4.5}, {11.1, 10.0}, {10.7, 10.6}};
 
-        i1d.printPairsThatIntersect(N, arrayWithPairOfDoubleValues);
+        i1dc.printPairsThatIntersect(N, arrayWithPairOfDoubleValues);
     }
 }
