15675450a1cd3ed23ef1e117e5c6535f8c135765
pop <popbojan@yahoo.com>
2019-03-29 05:39:06 +0100

    [E1211] Update description

--- src/main/java/book/exercises/E12_dataabstraction/E1211_SmartDate.java
+++ src/main/java/book/exercises/E12_dataabstraction/E1211_SmartDate.java
@@ -1,5 +1,9 @@
 package book.exercises.E12_dataabstraction;
 
+/**
+ * Exercise 1.2.11:
+ * Develop an implementation SmartDate of our Date API that raises an exception of the date is not legal
+ */
 public class E1211_SmartDate {
 
     public void createDate() {
