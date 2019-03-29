1d64e829a6b8b6b017030a47c2df48c9ec11ee10
pop <popbojan@yahoo.com>
2019-03-29 06:20:15 +0100

    [E129] Update description

--- src/main/java/book/exercises/E12_dataabstraction/E129_BinarySearchCounter.java
+++ src/main/java/book/exercises/E12_dataabstraction/E129_BinarySearchCounter.java
@@ -7,7 +7,7 @@ import video.examples.sort.MergeSort;
  * Exercise 1.2.9:
  * Instrument BinarySearch (page 47) to use a Counter to count the total number
  * of keys examined during all searches and then print the total after all searches
- * are complete. Hint: Create a Counter in main() and pass it as an argument to rank()
+ * are complete. Hint: Create a Counter in main() and pass it as an argument to rank().
  */
 public class E129_BinarySearchCounter {
 
