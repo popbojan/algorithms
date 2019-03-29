1872c65f8fece88eb92ea00b1985ddec43af3e63
pop <popbojan@yahoo.com>
2019-03-29 06:20:44 +0100

    [E1211] Update description

--- src/main/java/book/exercises/E12_dataabstraction/E1211_SmartDate.java
+++ src/main/java/book/exercises/E12_dataabstraction/E1211_SmartDate.java
@@ -5,7 +5,12 @@ import book.exercises.E12_dataabstraction.helper.SmartDateException;
 
 /**
  * Exercise 1.2.11:
- * Develop an implementation SmartDate of our Date API that raises an exception of the date is not legal
+ * Develop an implementation SmartDate of our Date API that raises an exception of the date is not legal.
+ *
+ * Exercise 1.2.12:
+ * Add a method dayOfTheWeek() to SmartDate that returns a String value Monday, Tuesday, Wednesday,
+ * Thursday, Friday, Saturday or Sunday, giving the appropriate day of the week for the date.
+ * You may assume that the date is in the 21st century.
  */
 public class E1211_SmartDate {
 
