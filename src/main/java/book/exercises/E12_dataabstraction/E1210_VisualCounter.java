5bfd798433c6d0b67e58f6bc6916fbfb61caf181
pop <popbojan@yahoo.com>
2019-03-29 05:37:39 +0100

    [E1210] Update description

--- src/main/java/book/exercises/E12_dataabstraction/E1210_VisualCounter.java
+++ src/main/java/book/exercises/E12_dataabstraction/E1210_VisualCounter.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.Random;
 
 /**
+ * Exercise 1.2.10:
  * Develop a class VisualCounter that allows both increment and decrement operations.
  * Take two arguments N and max in the constructor, where N specifies the maximum number
  * of operations and max specifies the maximum absolute value for the counter. As a side
