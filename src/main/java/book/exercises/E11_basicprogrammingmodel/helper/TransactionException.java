2307d1229d7f89c89c702c33f1e73e7ea42fa6f7
pop <popbojan@yahoo.com>
2019-04-02 05:52:42 +0200

    [E1214] Move TransactionException to helper package (fix)

---
+++ src/main/java/book/exercises/E11_basicprogrammingmodel/helper/TransactionException.java
@@ -0,0 +1,9 @@
+package book.exercises.E11_basicprogrammingmodel.helper;
+
+public class TransactionException extends Exception {
+
+    public TransactionException(String message) {
+        super(message);
+    }
+
+}
