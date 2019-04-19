31d203ffd07b4866fcc807b9953bf425c09f6287
pop <popbojan@yahoo.com>
2019-04-19 07:12:42 +0200

    [E1218] Update description

--- src/main/java/book/exercises/E12_dataabstraction/E1218_VarianceForAccumulator.java
+++ src/main/java/book/exercises/E12_dataabstraction/E1218_VarianceForAccumulator.java
@@ -4,7 +4,7 @@ package book.exercises.E12_dataabstraction;
  * Exercise 1.2.18:
  * Variance for accumulator. Validate that the following code, which adds the methods
  * var() and stddev() to Accumulator, computes both the mean and variance of the numbers
- * presented as arguments to addDateValue():
+ * presented as arguments to addDataValue():
  * <p>
  * public class Accumulator
  * {
@@ -12,7 +12,7 @@ package book.exercises.E12_dataabstraction;
  * private double s;
  * private int N;
  * <p>
- * public void addDateValue(double x){
+ * public void addDataValue(double x){
  * N++;
  * s = s + 1.0 * (N - 1) / N * (x - m) * (x - m);
  * m = m + (x - m) / N;
@@ -30,6 +30,17 @@ package book.exercises.E12_dataabstraction;
  * }
  * This implementation is less susceptible to roundoff error than the straightforward
  * implementation based on saving the sum of the squares of the numbers.
+ * <p>
+ * The Accumulator API shown on the page 93 defines an abstract data type that provides
+ * to client the ability to maintain a running average of data values. The implementation
+ * is straightforward. It maintains a int instance variable counts the number of data
+ * values seen so far and a double instance variable that keeps track of the sum of the
+ * values seen so far. To compute the average it divides the sum by the count. Note that
+ * the implementation does not save the data values - it could be used for a huge number
+ * of them (even on a device that is not capable of holding that many), or a huge number
+ * of accumulators could be used on a big system. This performance characteristic is
+ * subtle and might be specified in the API, because an implementation that does save the
+ * values might cause an application to run out of memory.
  */
 public class E1218_VarianceForAccumulator {
 
---
+++ src/main/java/book/exercises/E12_dataabstraction/helper/Accumulator.java
@@ -0,0 +1,27 @@
+package book.exercises.E12_dataabstraction.helper;
+
+public class Accumulator {
+    private double m;
+    private double s;
+    private int N;
+
+
+    public void addDataValue(double x) {
+        N++;
+        s = s + 1.0 * (N - 1) / N * (x - m) * (x - m);
+        m = m + (x - m) / N;
+    }
+
+
+    public double mean() {
+        return m;
+    }
+
+    public double var() {
+        return s / (N - 1);
+    }
+
+    public double stddev() {
+        return Math.sqrt(this.var());
+    }
+}
