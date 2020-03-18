1b79f9df6801a370f36218833e21307191e191e3
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-18 09:04:52 +0100

    [E133] Start skeleton for intermixed sequence of stack operations

---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E_133_IntermixedSequenceOfStackOperationsTests.java
@@ -0,0 +1,20 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E_133_IntermixedSequenceOfStackOperations;
+import org.junit.Before;
+import org.junit.Test;
+
+public class E_133_IntermixedSequenceOfStackOperationsTests {
+
+    private E_133_IntermixedSequenceOfStackOperations isoso;
+
+    @Before
+    public void setup(){
+        isoso = new E_133_IntermixedSequenceOfStackOperations();
+    }
+
+    @Test
+    public void test_wrongSequence_sequenceCannotOccur(){
+
+    }
+}
