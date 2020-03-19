ed845542d30fa48c7b7cd5b63d82c423167b8730
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-19 09:02:46 +0100

    [E134] Introduce skeleton for 1.3.4. exercise

---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E_134_ParenthesesTests.java
@@ -0,0 +1,26 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E_134_Parentheses;
+import org.junit.Before;
+import org.junit.Test;
+
+public class E_134_ParenthesesTests {
+
+    E_134_Parentheses p;
+
+    @Before
+    public void setup(){
+
+        p  = new E_134_Parentheses();
+    }
+
+    @Test
+    public void test_parenthesesProperlyBalanced_returnsTrue(){
+
+    }
+
+    @Test
+    public void test_parenthesesProperlyBalanced_wrongInput_returnsFalse(){
+
+    }
+}
