54fba18a498da8760f9146108f962fe2251d97f0
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-19 09:02:54 +0100

    [E134] Introduce skeleton for 1.3.4. exercise

---
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E_134_Parentheses.java
@@ -0,0 +1,16 @@
+package book.exercises.E13_bagsqueuesandstacks;
+/**
+ * Exercise 1.3.4:
+ * Write stack client Parentheses that reads in a text stream from standard input and uses a stack to determine whether its parentheses are properly balanced.
+ * For example, you program should print true for [()]{}{[()()]()} and false for [(]).
+ *
+ * checkout page 129 in the book
+ */
+public class E_134_Parentheses {
+
+    public E_134_Parentheses(){}
+
+    public boolean parenthesesProperlyBalanced(){
+        throw new RuntimeException("Not implemented yet");
+    }
+}
