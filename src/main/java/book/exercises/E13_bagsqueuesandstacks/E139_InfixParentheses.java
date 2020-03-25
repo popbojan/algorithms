21ae7e4a397f9d9e27e463f4180e7941f8237068
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-25 07:52:40 +0100

    [E139] Introduce skeleton

---
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E139_InfixParentheses.java
@@ -0,0 +1,16 @@
+package book.exercises.E13_bagsqueuesandstacks;
+
+/**
+ * Exercise 1.3.9:
+ * Write a program that takes from standard input an expression without left parentheses and prints
+ * the equivalent infix expression with the parentheses inserted. For example, given the input:
+ * 1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )
+ * your program should print
+ * ( ( 1 + 2 ) * ( ( 3 - 4 ) * ( 5 - 6 ) ) )
+ */
+public class E139_InfixParentheses {
+
+    public E139_InfixParentheses(){
+
+    }
+}
---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E139_InfixParenthesesTests.java
@@ -0,0 +1,20 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E139_InfixParentheses;
+import org.junit.Before;
+import org.junit.Test;
+
+public class E139_InfixParenthesesTests {
+
+    private E139_InfixParentheses ip;
+
+    @Before
+    public void setup(){
+        ip = new E139_InfixParentheses();
+    }
+
+    @Test
+    public void test_infix_returnsRightArithmeticExpression(){
+
+    }
+}
