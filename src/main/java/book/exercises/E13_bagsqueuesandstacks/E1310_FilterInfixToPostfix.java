16b825e1f6a3d20330638f59c282489a5dd149bc
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-26 07:36:52 +0100

    [E1310] Add link to tutorial

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
@@ -3,6 +3,9 @@ package book.exercises.E13_bagsqueuesandstacks;
 /**
  * Exercise 1.3.10:
  * Write a filter InfixToPostfix that converts an arithmetic expression from infix to postfix
+ *
+ * Check this link out:
+ * https://runestone.academy/runestone/books/published/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
  */
 public class E1310_FilterInfixToPostfix {
 
