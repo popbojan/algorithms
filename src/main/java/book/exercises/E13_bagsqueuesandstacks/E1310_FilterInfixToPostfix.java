f50c30a9ed42fb851ecc360b46a894616d17d520
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-26 07:11:56 +0100

    [E139] Introduce skeleton

---
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
@@ -0,0 +1,10 @@
+package book.exercises.E13_bagsqueuesandstacks;
+
+/**
+ * Exercise 1.3.10:
+ * Write a filter InfixToPostfix that converts an arithmetic expression from infix to postfix
+ */
+public class E1310_FilterInfixToPostfix {
+
+    public E1310_FilterInfixToPostfix(){}
+}
---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfixTests.java
@@ -0,0 +1,20 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E1310_FilterInfixToPostfix;
+import org.junit.Before;
+import org.junit.Test;
+
+public class E1310_FilterInfixToPostfixTests {
+
+    private E1310_FilterInfixToPostfix fitp;
+
+    @Before
+    public void setup(){
+        fitp = new E1310_FilterInfixToPostfix();
+    }
+
+    @Test
+    public void test_filter_returnsPostfix(){
+
+    }
+}
