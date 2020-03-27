441740da6d891f06a9bc24b175c1231657885acb
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-27 09:27:22 +0100

    [E139] Add infix -> prefix -> postfix examples within description

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
@@ -6,6 +6,15 @@ package book.exercises.E13_bagsqueuesandstacks;
  *
  * Check this link out:
  * https://runestone.academy/runestone/books/published/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
+ *
+ * Infix -> Prefix -> Postfix Examples:
+ * infix -> A + B                     prefix -> + A B               postfix -> A B +
+ * infix -> A + B * C                 prefix -> + A * B C           postfix -> A B C * +
+ * infix -> A + B * C + D             prefix -> + + A * B C D       postfix -> A B C * + D +
+ * infix -> (A + B) * (C + D)         prefix -> * + A B + C D       postfix -> A B + C D + *
+ * infix -> A * B + C * D             prefix -> + * A B * C D       postfix -> A B * C D * +
+ * infix -> A + B + C + D             prefix -> + + + A B C D       postfix -> A B + C + D +
+ * infix -> ( ( 1 + 2 ) * ( 4 / 2 ) ) prefix -> ?                   postfix -> 1 2 + 4 2 / *
  */
 public class E1310_FilterInfixToPostfix {
 
