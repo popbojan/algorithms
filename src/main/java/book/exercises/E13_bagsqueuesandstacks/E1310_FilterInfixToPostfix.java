0161cbb649b4217a1eda364e08d4ccced5bee0f8
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-29 09:04:42 +0200

    [E1310] Implement solution

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfix.java
@@ -1,12 +1,14 @@
 package book.exercises.E13_bagsqueuesandstacks;
 
+import book.exercises.E13_bagsqueuesandstacks.helper.JavaStack;
+
 /**
  * Exercise 1.3.10:
  * Write a filter InfixToPostfix that converts an arithmetic expression from infix to postfix
- *
+ * <p>
  * Check this link out:
  * https://runestone.academy/runestone/books/published/pythonds/BasicDS/InfixPrefixandPostfixExpressions.html
- *
+ * <p>
  * Infix -> Prefix -> Postfix Examples:
  * infix -> A + B                     prefix -> + A B               postfix -> A B +
  * infix -> A + B * C                 prefix -> + A * B C           postfix -> A B C * +
@@ -18,5 +20,49 @@ package book.exercises.E13_bagsqueuesandstacks;
  */
 public class E1310_FilterInfixToPostfix {
 
-    public E1310_FilterInfixToPostfix(){}
+    private JavaStack<String> operands;
+    private JavaStack<String> operators;
+
+    public E1310_FilterInfixToPostfix() {
+        operands = new JavaStack<>();
+        operators = new JavaStack<>();
+    }
+
+    /**
+     * handles only parenthesized infix expression
+     *
+     * @param inputExpression
+     * @return postfix expression
+     */
+    public String infixToPostfix(String inputExpression) {
+
+        String[] inputValues = inputExpression.split("\\s");
+
+        for (String value : inputValues) {
+            if (value.equals("(")) {
+
+            } else if (value.equals("+")
+                    || value.equals("-")
+                    || value.equals("*")
+                    || value.equals("/")) {
+                operators.pushToStack(value);
+            } else if (value.equals(")")) {
+                String operator = operators.popFromStack();
+                String value2 = operands.popFromStack();
+                String value1 = operands.popFromStack();
+
+                operands.pushToStack(value1 + " " + value2 + " " + operator);
+
+            } else {
+                operands.pushToStack(value);
+            }
+        }
+
+        String result = "";
+        for (String operand : operands) {
+            result += operand;
+        }
+
+        return result;
+    }
 }
--- src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfixTests.java
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1310_FilterInfixToPostfixTests.java
@@ -1,6 +1,7 @@
 package unit.book.exercises.E13_bagsqueuesandstacks;
 
 import book.exercises.E13_bagsqueuesandstacks.E1310_FilterInfixToPostfix;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,12 +10,21 @@ public class E1310_FilterInfixToPostfixTests {
     private E1310_FilterInfixToPostfix fitp;
 
     @Before
-    public void setup(){
+    public void setup() {
         fitp = new E1310_FilterInfixToPostfix();
     }
 
     @Test
-    public void test_filter_returnsPostfix(){
-
+    public void test_infixToPostfix_returnsPostfix() {
+        String output = fitp.infixToPostfix("( ( 1 + 2 ) * ( 4 / 2 ) )");
+        Assert.assertEquals("1 2 + 4 2 / *", output);
     }
+
+    @Test
+    public void test_infixToPostfix_anotherExample_returnsPostfix() {
+        String output = fitp.infixToPostfix("( ( A + B ) * ( C + D ) )");
+        Assert.assertEquals("A B + C D + *", output);
+    }
+
+
 }
