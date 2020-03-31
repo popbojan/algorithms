894657693196d931986add9f3d58824eefd716ed
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-31 07:09:46 +0200

    [E1313] Introduce skeleton

---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1313_IntermixedSequenceOfQueueOperationsTests.java
@@ -0,0 +1,25 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E1313_IntermixedSequenceOfQueueOperations;
+import org.junit.Before;
+import org.junit.Test;
+
+public class E1313_IntermixedSequenceOfQueueOperationsTests {
+
+    private E1313_IntermixedSequenceOfQueueOperations isoqo;
+
+    @Before
+    public void setup() {
+        isoqo = new E1313_IntermixedSequenceOfQueueOperations();
+    }
+
+    @Test
+    public void test_aScenario_validSequence() {
+
+    }
+
+    @Test
+    public void test_bScenario_invalidSequence() {
+
+    }
+}
