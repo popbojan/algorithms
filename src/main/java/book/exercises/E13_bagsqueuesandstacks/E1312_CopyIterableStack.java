93e8efa51a1f336544af38441b5e240531198785
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-31 06:57:37 +0200

    [E1312] Implement copy solution

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/E1312_CopyIterableStack.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1312_CopyIterableStack.java
@@ -1,6 +1,6 @@
 package book.exercises.E13_bagsqueuesandstacks;
 
-import book.exercises.E13_bagsqueuesandstacks.helper.JavaStack;
+import book.exercises.E13_bagsqueuesandstacks.helper.ResizingArrayStack;
 
 /**
  * Exercise 1.3.12:
@@ -9,12 +9,25 @@ import book.exercises.E13_bagsqueuesandstacks.helper.JavaStack;
  * because it allows development of such functionality without changing the basic API.
  * Check out page 141 in the book
  */
-public class E1312_CopyIterableStack {
+public class E1312_CopyIterableStack<Item> {
+
+    private ResizingArrayStack<Item> temp;
+    private ResizingArrayStack<Item> newStack;
 
     public E1312_CopyIterableStack() {
 
     }
 
-    public void copy(JavaStack<String> stack) {
+    public ResizingArrayStack<Item> copy(ResizingArrayStack<Item> stack) {
+        temp = new ResizingArrayStack<>();
+        newStack = new ResizingArrayStack<>();
+        // use temp stack in order to keep an order of items
+        for (Item item : stack) {
+            temp.push(item);
+        }
+        for (Item item : temp) {
+            newStack.push(item);
+        }
+        return newStack;
     }
 }
--- src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1312_CopyIterableStackTests.java
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1312_CopyIterableStackTests.java
@@ -1,28 +1,55 @@
 package unit.book.exercises.E13_bagsqueuesandstacks;
 
 import book.exercises.E13_bagsqueuesandstacks.E1312_CopyIterableStack;
-import book.exercises.E13_bagsqueuesandstacks.helper.JavaStack;
+import book.exercises.E13_bagsqueuesandstacks.helper.ResizingArrayStack;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class E1312_CopyIterableStackTests {
 
-    private E1312_CopyIterableStack cis;
-    private JavaStack<String> stack;
+    private E1312_CopyIterableStack<String> cisos;
+    private E1312_CopyIterableStack<Integer> cisoi;
+    private ResizingArrayStack<String> stack;
+    private ResizingArrayStack<Integer> stackOfIntegers;
 
     @Before
     public void setup() {
-        cis = new E1312_CopyIterableStack();
-        stack = new JavaStack<>();
+        cisos = new E1312_CopyIterableStack();
+        cisoi = new E1312_CopyIterableStack<>();
+        stack = new ResizingArrayStack<>();
+        stackOfIntegers = new ResizingArrayStack<>();
     }
 
     @Test
     public void test_copy_returnsSameStack() {
-        stack.pushToStack("first");
-        stack.pushToStack("second");
-        stack.pushToStack("third");
-        stack.pushToStack("forth");
-        stack.pushToStack("fifth");
-        cis.copy(stack);
+        stack.push("first");
+        stack.push("second");
+        stack.push("third");
+        stack.push("forth");
+        stack.push("fifth");
+        ResizingArrayStack<String> copy = cisos.copy(stack);
+        Assert.assertEquals(copy.size(), stack.size());
+        Assert.assertEquals(copy.pop(), stack.pop());
+        Assert.assertEquals(copy.pop(), stack.pop());
+        Assert.assertEquals(copy.pop(), stack.pop());
+        Assert.assertEquals(copy.pop(), stack.pop());
+        Assert.assertEquals(copy.pop(), stack.pop());
+    }
+
+    @Test
+    public void test_copyIntegers_returnsSameStack() {
+        stackOfIntegers.push(1);
+        stackOfIntegers.push(2);
+        stackOfIntegers.push(3);
+        stackOfIntegers.push(4);
+        stackOfIntegers.push(5);
+        ResizingArrayStack<Integer> copy = cisoi.copy(stackOfIntegers);
+        Assert.assertEquals(copy.size(), stackOfIntegers.size());
+        Assert.assertEquals(copy.pop(), stackOfIntegers.pop());
+        Assert.assertEquals(copy.pop(), stackOfIntegers.pop());
+        Assert.assertEquals(copy.pop(), stackOfIntegers.pop());
+        Assert.assertEquals(copy.pop(), stackOfIntegers.pop());
+        Assert.assertEquals(copy.pop(), stackOfIntegers.pop());
     }
 }
