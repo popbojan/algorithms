0c9b97090bdae3fb24b90d6bf46fc297d642de8f
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-03-31 09:00:32 +0200

    [E1314] Implement enqueue and dequeue with resizing

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/helper/ResizingArrayQueue.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/helper/ResizingArrayQueue.java
@@ -4,9 +4,56 @@ import java.util.Iterator;
 
 public class ResizingArrayQueue<Item> implements Iterable<Item> {
 
+    private Item[] a;
+    private int N = 0;
+    private int index = 0;
+
+    public ResizingArrayQueue() {
+        a = (Item[]) new Object[7];
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public void enqueue(Item item) {
+        if (N == a.length) {
+            resizeArray(a.length * 2);
+        }
+        a[N++] = item;
+    }
+
+    public Item dequeue() {
+        Item item = a[index];
+        a[index++] = null;
+        N--;
+        if (a.length / 4 > N) {
+            resizeArray(a.length / 2);
+        }
+        return item;
+    }
+
+    public int arraySize() {
+        return a.length;
+    }
+
     @Override
     public Iterator<Item> iterator() {
         return new ArrayIterator();
+    }
+
+    private void resizeArray(int maxSize) {
+        Item[] temp = (Item[]) new Object[maxSize];
+        for (int i = 0; i < maxSize && index < N; i++) {
+            temp[i] = a[index];
+            a[index++] = null;
+        }
+        a = temp;
+        index = 0;
     }
 
     private class ArrayIterator implements Iterator<Item> {
--- src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1314_ResizingArrayQueueTests.java
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1314_ResizingArrayQueueTests.java
@@ -1,20 +1,49 @@
 package unit.book.exercises.E13_bagsqueuesandstacks;
 
 import book.exercises.E13_bagsqueuesandstacks.E1314_ResizingArrayQueue;
+import book.exercises.E13_bagsqueuesandstacks.helper.ResizingArrayQueue;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class E1314_ResizingArrayQueueTests {
 
-    private E1314_ResizingArrayQueue raq;
+    private E1314_ResizingArrayQueue<String> raq;
+    private ResizingArrayQueue<String> queue;
 
     @Before
     public void setup() {
         raq = new E1314_ResizingArrayQueue();
+        queue = new ResizingArrayQueue<>();
     }
 
     @Test
-    public void test_queue_works() {
+    public void test_queue_doesEnqueueDequeueProperly() {
+        queue.enqueue("eins");
+        queue.enqueue("zwei");
+        queue.enqueue("drei");
+        queue.enqueue("vier");
+        queue.enqueue("fünf");
+        queue.enqueue("sechs");
+        queue.enqueue("sieben");
+        queue.enqueue("acht");
+        queue.enqueue("neun");
+        queue.enqueue("zehn");
+        queue.enqueue("elf");
+        queue.enqueue("zwölf");
+
+        //Assert.assertEquals(3, queue.size());
+        Assert.assertEquals("eins", queue.dequeue());
+        Assert.assertEquals("zwei", queue.dequeue());
+        Assert.assertEquals("drei", queue.dequeue());
+        Assert.assertEquals("vier", queue.dequeue());
+        Assert.assertEquals("fünf", queue.dequeue());
+        Assert.assertEquals("sechs", queue.dequeue());
+        Assert.assertEquals("sieben", queue.dequeue());
+        Assert.assertEquals("acht", queue.dequeue());
+        //Assert.assertEquals(0, queue.size());
+        //Assert.assertEquals(7, queue.arraySize());
+
 
     }
 }
