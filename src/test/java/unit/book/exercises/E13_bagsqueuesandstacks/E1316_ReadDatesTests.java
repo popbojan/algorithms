5c7450eef770c28d0bbc08840d4f8aad7fd1772b
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-04-02 11:57:07 +0200

    [E1316] Introduce skeleton

---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1316_ReadDatesTests.java
@@ -0,0 +1,19 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E1316_ReadDates;
+import org.junit.Before;
+import org.junit.Test;
+
+public class E1316_ReadDatesTests {
+
+    private E1316_ReadDates rd;
+
+    @Before
+    public void setup() {
+        rd = new E1316_ReadDates();
+    }
+
+    @Test
+    public void test_something_doesSomething() {
+    }
+}
