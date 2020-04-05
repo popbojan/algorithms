34d30d93c0d17885c56c496cfca9d55c7fbb12e6
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-04-05 08:28:36 +0200

    [E1316] Implement solution

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/E1316_ReadDates.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1316_ReadDates.java
@@ -1,5 +1,11 @@
 package book.exercises.E13_bagsqueuesandstacks;
 
+import book.exercises.E12_dataabstraction.helper.SmartDate;
+import book.exercises.E12_dataabstraction.helper.exception.SmartDateException;
+import book.exercises.E13_bagsqueuesandstacks.helper.ResizingArrayQueue;
+
+import java.util.List;
+
 /**
  * Exercise 1.3.16:
  * Using readInts() on page 126 as a model, write a static method readDates() for Date that reads dates from standard
@@ -7,6 +13,25 @@ package book.exercises.E13_bagsqueuesandstacks;
  */
 public class E1316_ReadDates {
 
+    ResizingArrayQueue<String> queue;
+
     public E1316_ReadDates() {
+        queue = new ResizingArrayQueue();
+    }
+
+    public String[] readDates(List<String> dates) throws SmartDateException {
+        for (String date : dates) {
+            SmartDate smartDate = SmartDate.getInstance(date);
+            if (smartDate != null) {
+                queue.enqueue(date);
+            }
+        }
+
+        int N = queue.size();
+        String[] datesArray = new String[N];
+        for (int i = 0; i < N; i++) {
+            datesArray[i] = queue.dequeue();
+        }
+        return datesArray;
     }
 }
--- src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1316_ReadDatesTests.java
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1316_ReadDatesTests.java
@@ -1,8 +1,13 @@
 package unit.book.exercises.E13_bagsqueuesandstacks;
 
+import book.exercises.E12_dataabstraction.helper.exception.SmartDateException;
 import book.exercises.E13_bagsqueuesandstacks.E1316_ReadDates;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class E1316_ReadDatesTests {
 
@@ -14,6 +19,31 @@ public class E1316_ReadDatesTests {
     }
 
     @Test
-    public void test_something_doesSomething() {
+    public void test_readDates_returnsDatesArray() throws SmartDateException {
+        List<String> dates = new ArrayList<>();
+        dates.add("4/5/2020");
+        dates.add("2/25/1960");
+        dates.add("08/12/1984");
+
+        String[] result = rd.readDates(dates);
+
+        Assert.assertEquals(3, result.length);
+        Assert.assertEquals("08/12/1984", result[2]);
+        Assert.assertEquals("2/25/1960", result[1]);
     }
+
+    @Test
+    public void test_readDates_invalidDate_throwsException() throws SmartDateException {
+        List<String> dates = new ArrayList<>();
+        dates.add("14/02/2020");
+        dates.add("2/25/1960");
+        dates.add("08/12/1984");
+
+        try {
+            String[] result = rd.readDates(dates);
+        } catch (SmartDateException ex) {
+            Assert.assertEquals("Date is not valid", ex.getMessage());
+        }
+    }
+
 }
