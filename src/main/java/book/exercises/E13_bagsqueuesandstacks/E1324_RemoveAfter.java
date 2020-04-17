794b1cfb076b30929554c8b5bf3c0241569eec00
Bojan Popovic <bojan.popovic@libertexgroup.com>
2020-04-17 12:56:24 +0200

    [E1324] Fix typo in RemoveAfter exercise description

--- src/main/java/book/exercises/E13_bagsqueuesandstacks/E1324_RemoveAfter.java
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1324_RemoveAfter.java
@@ -5,7 +5,7 @@ import book.exercises.E13_bagsqueuesandstacks.helper.MyLinkedList;
 /**
  * Exercise 1.3.24:
  * Write a method removeAfter() that takes a linked-list Node as argument and removes the node following the given one
- * (and does niothing if the argument or the nex field in the argument node is null).
+ * (and does nothing if the argument or the nex field in the argument node is null).
  */
 public class E1324_RemoveAfter {
 
