f05042b00ced38f8f5c576bd21da141cb0d02e19
Bojan Popovic <popbojan@Bojans-MacBook-Pro.local>
2020-10-16 22:16:27 +0200

    [E1331] Test if commiting from MY new MAC works. Skleton of DoubleLinkedList

---
+++ src/main/java/book/exercises/E13_bagsqueuesandstacks/E1331_DoubleLinkedList.java
@@ -0,0 +1,16 @@
+package book.exercises.E13_bagsqueuesandstacks;
+
+/**
+ * Exercise 1.3.30:
+ * Implement a nested class DoubleNode for building doubly-linked lists, where each node contains a reference to the
+ * item preceding it and the item following it in the list (null if there is no such item). Then implement static
+ * methods for the following tasks: insert at the beginning, insert at the end, remove from the beginning, remove from
+ * the end, insert before a given node, insert after a given node, and remove a given node.
+ */
+public class E1331_DoubleLinkedList {
+
+    public E1331_DoubleLinkedList(){
+
+    }
+
+}
---
+++ src/test/java/unit/book/exercises/E13_bagsqueuesandstacks/E1331_DoubleLinkedListTests.java
@@ -0,0 +1,22 @@
+package unit.book.exercises.E13_bagsqueuesandstacks;
+
+import book.exercises.E13_bagsqueuesandstacks.E1331_DoubleLinkedList;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class E1331_DoubleLinkedListTests {
+
+    private E1331_DoubleLinkedList dll;
+
+    @Before
+    public void setup() {
+        dll = new E1331_DoubleLinkedList();
+    }
+
+    @Test
+    public void test_something() {
+        assertEquals(1, 1);
+    }
+}
